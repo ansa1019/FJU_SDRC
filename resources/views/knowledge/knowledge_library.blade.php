60315ddd9ab5409bf5a6ad97e427757878cc5a1a
B10856021 <qazqwer1355@gmail.com>
2024-09-23 18:24:50 +0800

    收藏：圖片還未修改好。 路徑：還未修正作者、文章收藏部分

--- resources/views/knowledge/knowledge_library.blade.php
+++ resources/views/knowledge/knowledge_library.blade.php
@@ -17,7 +17,7 @@
                     <!-- 麵包屑 網頁導覽列-->
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="#">知識圖書館</a></li>
-                        <li class="breadcrumb-item"><a href="{{ url('/knowledge_library/' . $category . '/' . $subcategory) }}">查看{{ $category }} / {{ $subcategory }}</a>
+                        <li class="breadcrumb-item"><a href="{{ url('/knowledge_library/' . $category . '/' . $subcategory) }}">{{ $category }}</a>
 
                         </li>
                         <li class="breadcrumb-item active" aria-current="page">{{ $subcategory }}</li>
