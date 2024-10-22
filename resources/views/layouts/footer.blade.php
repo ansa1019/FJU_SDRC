fbccee65a2d58fa17ca1d98317bb9d305ecd089b
ansa1019 <ansa1019@gmail.com>
2024-10-22 20:26:11 +0800

    頁尾格式

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,39 +1,43 @@
 <footer class="footer-px ct-bg-3 mt-5">
-    <div class="row p-xs-2 p-md-4 px-lg-4 text-center justify-content-md-center justify-content-lg-evenly">
-        <div class="col">
-            <a href="https://www.uterusally.com/" target="_blank" class="">優德莎莉官網</a>
+    <div class="row">
+        <div class="col-5 col-lg-3">
+            <h6 class="row">關於我們</h6>
+            <a class="row" href="https://www.uterusally.com/" target="_blank">優德莎莉官網</a>
+            <a class="row" href="{{ url('/tos') }}">服務條款</a>
+            <a class="row" href="{{ url('/privacy') }}">隱私權政策</a>
+            <div class="d-flex d-lg-none row">
+                <a href="https://www.facebook.com/uterusally/" target="_blank" class="mr-3">
+                    <i class="fab fa-facebook-square"></i>
+                </a>
+                <a href="https://www.instagram.com/uterusally/" target="_blank" class="mx-3">
+                    <i class="fab fa-instagram"></i>
+                </a>
+                <a href="https://lin.ee/9uEFur8" target="_blank" class="ml-3">
+                    <i class="fab fa-line"></i>
+                </a>
+            </div>
         </div>
-        <div class="col">
-            <a href="{{ url('/tos') }}" class="">服務條款</a>
-        </div>
-        <div class="col">
-            <a href="{{ url('/privacy') }}" class="">隱私權政策</a>
-        </div>
-
-        <div class="col-12 col-md-auto p-3 p-md-0">
-            <a href="https://www.facebook.com/uterusally/" target="_blank" class="col-12 ct-txt-2 mx-3 mx-md-2">
-                <i class="fab fa-facebook-square"></i>
-            </a>
-            <a href="https://www.instagram.com/uterusally/" target="_blank" class="col-12 ct-txt-2 mx-3 mx-md-2">
-                <i class="fab fa-instagram"></i>
-            </a>
-            <a href="https://lin.ee/9uEFur8" target="_blank" class="col-12 ct-txt-2 mx-3 mx-md-2">
-                <i class="fab fa-line"></i>
-            </a>
-        </div>
-    </div>
-    <div class="row p-2 text-center justify-content-md-center justify-content-lg-evenly">
-        <div class="col-12">
-            <h6>聯絡我們</h6>
-        </div>
-        <div class="col-12 d-flex flex-wrap justify-content-between text-nowrap" style="font-size: var(--fs-17);">
-            <div class="col m-2 m-lg-3">客服電話：<a href="tel:0800588668"
+        <div class="col-7 col-lg-auto mx-auto">
+            <h6 class="row">聯絡我們</h6>
+            <div class="row">客服電話：<a href="tel:0800588668"
                     style="text-decoration: none; color: inherit;">0800-588-668</a></div>
-            <div class="col m-2 m-lg-3">營業時間：AM 9:30 ~ PM 6:00</div>
-            <div class="col m-2 m-lg-3">客服信箱：<a href="mailto:service.uterusally@gmail.com"
-                    style="text-decoration: none; color: inherit;">service.uterusally@gmail.com</a></div>
-            <div class="col m-2 m-lg-3">地址：<a
-                    href="https://www.google.com/maps/search/?api=1&query=新北市新莊區新北大道3段222號8樓" target="_blank"
-                    style="text-decoration: none; color: inherit;">新北市新莊區新北大道3段222號8樓</a></div>
+            <div class="row">營業時間：AM 9:30 ~ PM 6:00</div>
+            <p class="row">客服信箱：<a href="mailto:service.uterusally@gmail.com"
+                    style="text-decoration: none; color: inherit;">service.uterusally@gmail.com</a></p>
+            <p class="row">地址：<a href="https://www.google.com/maps/search/?api=1&query=新北市新莊區新北大道3段222號8樓"
+                    target="_blank" style="text-decoration: none; color: inherit;">新北市新莊區新北大道3段222號8樓</a></p>
+        </div>
+        <div class="d-none d-lg-flex col-lg-3">
+            <div class="row justify-content-center">
+                <a href="https://www.facebook.com/uterusally/" target="_blank" class="mx-3 mx-md-2">
+                    <i class="fab fa-facebook-square"></i>
+                </a>
+                <a href="https://www.instagram.com/uterusally/" target="_blank" class="mx-3 mx-md-2">
+                    <i class="fab fa-instagram"></i>
+                </a>
+                <a href="https://lin.ee/9uEFur8" target="_blank" class="mx-3 mx-md-2">
+                    <i class="fab fa-line"></i>
+                </a>
+            </div>
         </div>
 </footer>
